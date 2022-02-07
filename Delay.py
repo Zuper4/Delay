c45bb8b2ed8cff9ad04661427907c313c6d7d947
Zuper <97028711+Zuper4@users.noreply.github.com>
2022-02-07 09:26:24 +0000

    Add files via upload

---
+++ Delay.py
@@ -0,0 +1,10 @@
+print("\nThis program will wait for a given time and then print a message.")
+
+import time
+
+x = int(input("\nHow many seconds would you like for the program to wait :"))
+
+time.sleep(x)
+
+print("\nFinally! I can talk.")
+
